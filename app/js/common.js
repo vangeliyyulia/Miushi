b8a79d85f7f77a22871db7c25c12378708da3c31
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-11-04 14:11:57 +0300

    Mobile Menu added

--- app/js/common.js
+++ app/js/common.js
@@ -3,26 +3,16 @@
     var hamburger = {
       navToggle: document.querySelector('.btn_mnu'),
       nav: document.querySelector('.nav__list'),
+      contacts: document.querySelector('.contacts-wrapper'),
       doToggle: function (e) {
         e.preventDefault();
         this.navToggle.classList.toggle('active');
         this.nav.classList.toggle('active');
+        this.contacts.classList.toggle('active');
       }
     };
     hamburger.navToggle.addEventListener('click', function (e) { hamburger.doToggle(e); });
     hamburger.nav.addEventListener('click', function (e) { hamburger.doToggle(e); });
   }());
 
-(function () {
-    var ham = {
-      navToggle: document.querySelector('.btn_mnu'),
-      nav: document.querySelector('.contacts-wrapper'),
-      doToggle: function (e) {
-        e.preventDefault();
-        this.navToggle.classList.toggle('active');
-        this.nav.classList.toggle('active');
-      }
-    };
-    ham.navToggle.addEventListener('click', function (e) { ham.doToggle(e); });
-    ham.nav.addEventListener('click', function (e) { ham.doToggle(e); });
-}());
+
